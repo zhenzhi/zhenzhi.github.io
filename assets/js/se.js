6de529df9226dd640ef8fe6bddce2395ce4c6751
zhenzhi <ZZZ@zzz-macmac.attlocal.net>
2017-12-30 13:07:10 -0600

    ebr5
    tar

--- assets/js/se.js
+++ assets/js/se.js
@@ -8,17 +8,17 @@
 
 function passWord() {
 var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
+var pass1 = prompt('Please Enter Password',' ');
 while (testV < 3) {
 if (!pass1) 
 history.go(-1);
 if (pass1.toLowerCase() == "ux+") {
-window.open('hub.html');
+window.open('hub.html','_self');
 break;
 } 
 testV+=1;
 var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+prompt('Access Denied - Incorrect, Please Try Again.','Password');
 }
 if (pass1.toLowerCase()!="password" & testV ==3) 
 history.go(-1);
---
+++ assets/js/se4.js
@@ -0,0 +1,26 @@
+// SmoothScroll v0.9.9
+// Licensed under the terms of the MIT license.
+
+// People involved
+// - Balazs Galambosi: maintainer (CHANGELOG.txt)
+// - Patrick Brunner (patrickb1991@gmail.com)
+// - Michael Herf: ssc_pulse Algorithm
+
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Password',' ');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "ux+") {
+window.open('ais.html','_self');
+break;
+} 
+testV+=1;
+var pass1 = 
+prompt('Access Denied - Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+history.go(-1);
+return " ";
+} 
