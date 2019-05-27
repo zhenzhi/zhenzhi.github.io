f4766e875a3074cbab5de79d115f633b4682c679
zhenzhi <ZZZ@zzz-macmac.attlocal.net>
2019-05-27 16:18:28 -0500

    tsr
    8q71

--- assets/js/se.js
+++ assets/js/se.js
@@ -12,7 +12,7 @@ var pass1 = prompt('Please Enter Password',' ');
 while (testV < 3) {
 if (!pass1) 
 history.go(-1);
-if (pass1.toLowerCase() == "ux+") {
+if (pass1.toLowerCase() == "ats") {
 window.open('hub.html','_self');
 break;
 } 
