e27359a0cea4032489e728788e3512ad408ef03c
zhenzhi <ZZZ@zzz-macbook.attlocal.net>
2020-05-30 00:53:40 -0500

    wi8-1=n
    hsyw6791

---
+++ assets/js/hu.js
@@ -0,0 +1,21 @@
+// SmoothScroll v0.9.9
+// Licensed under the terms of the MIT license.
+
+// People involved
+// - Balazs Galambosi: maintainer (CHANGELOG.txt)
+// - Patrick Brunner (patrickb1991@gmail.com)
+// - Michael Herf: ssc_pulse Algorithm
+
+    
+    
+function TheLogin() {
+
+var password = 'ats';
+
+if (this.document.login.pass.value == password) {
+  top.location.href="hub.html";
+}
+else {
+  window.alert("Incorrect password, please try again.");
+  }
+}
